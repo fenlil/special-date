6daf87d75cc820b79442bb87d4d98fc281c38645
fenlil <68628783+fenlil@users.noreply.github.com>
2021-01-02 15:57:20 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const specialDate = {
   second: 0,
 };
 
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
 const data = { date: String(new Date()) };
 fetch('https://serverless.fenriuz.vercel.app/api/test/functionuwu', {
   method: 'POST',
@@ -27,12 +31,23 @@ const flexData = document.querySelectorAll('#flex-table h1');
 const updateClock = () => {
   const newDate = new Date();
 
+  const counterYear = (newDate.getMonth() - specialDate.month >= 0)
+    ? newDate.getFullYear() - specialDate.year
+    : newDate.getFullYear() - specialDate.year - 1;
+  let counterDays;
+  if (newDate.getMonth > 0) {
+    counterDays = daysInMonth(newDate.getMonth - 1, newDate.getFullYear());
+  } else {
+    counterDays = daysInMonth(11, newDate.getFullYear() - 1);
+  }
+  console.log('abr', daysInMonth(11, newDate.getFullYear - 1));
+
   const counterTime = [
-    newDate.getFullYear() - specialDate.year,
+    counterYear,
     newDate.getMonth() - specialDate.month >= 0
       ? newDate.getMonth() - specialDate.month
-      : newDate.getMonth(),
-    newDate.getDate(),
+      : newDate.getMonth() + (11 - specialDate.month),
+    counterDays,
     newDate.getHours(),
     newDate.getMinutes(),
     newDate.getSeconds(),
